eb2b9549600d0d296f9905c1195c0522c29f9be8
Adelya Almazbekova <114854004+qwalad@users.noreply.github.com>
2022-10-14 15:16:58 +0600

    Delete praktika3.py

--- praktika3.py
+++
@@ -1,3 +0,0 @@
-names_a = {"Nicholas", "Mercy", "Michelle", "John", "Nicholas", "Mercy", "Jeff", "Bosco", "Nicholas",  "Teddy", "Milly"}
-print(names_a.difference("Nicholas"))
-print(names_a.difference("Mercy"))
